568d59a2a57cdce277012d4f20d0e179832afe5d
Filip Vanderstappen <filip@vanderstappen.be>
2022-10-11 13:30:35 +0200

    Add computed field date_deadline and validity to Property Offer model

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class PropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -8,3 +8,19 @@ class PropertyOffer(models.Model):
     status = fields.Selection(string='Status',selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy=False)
     partner_id = fields.Many2one("res.partner", string="Buyer", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+
+    validity = fields.Integer(string="Offer validity", default=7)
+    date_deadline = fields.Date(string='Offer deadline', compute="_compute_date_deadline", inverse="_compute_validity")
+    @api.depends("create_date", "date_deadline")
+    def _compute_validity(self):
+        for record in self:
+            create_date = fields.Date.today() if not record.create_date else record.create_date.date()
+            date_deadline = fields.Date.today() if not record.date_deadline else record.date_deadline
+            delta = date_deadline - create_date
+            record.validity = delta.days
+
+    @api.depends("validity", "create_date")
+    def _compute_date_deadline(self):
+        for record in self:
+            create_date = fields.Date.today() if not record.create_date else record.create_date.date()
+            record.date_deadline = fields.Datetime.add(create_date, days=record.validity)
